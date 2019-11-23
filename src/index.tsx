ddd39a46e5cc1eb08cf520e589eb12040df9253d
inukko <lirabbits@yahoo.co.jp>
2019-11-24 01:08:03 +0900

    Typescript, Reactが使える環境を構築

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// TODO: ルーターを設定する
+import Top from './views/entrypoints/Top';
+
+ReactDOM.render(<Top />, document.getElementById('app'));
